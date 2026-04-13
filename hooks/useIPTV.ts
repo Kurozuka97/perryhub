c41e590eb78d28602046cee93f6d36fe6a8a06c8
Kaneda Shotaro <130251738+Kurozuka97@users.noreply.github.com>
2026-04-13 10:45:17 +0800

    Update useIPTV.ts

--- hooks/useIPTV.ts
+++ hooks/useIPTV.ts
@@ -18,6 +18,112 @@ const SOURCES = [
     label: 'Pluto TV',
     url: 'https://i.mjh.nz/PlutoTV/all.m3u8',
   },
+  [
+  {
+    "id": "iptv-org-master",
+    "label": "IPTV-org Master List (8,000+ channels)",
+    "url": "https://iptv-org.github.io/iptv/index.m3u"
+  },
+  {
+    "id": "free-tv-iptv",
+    "label": "Free-TV IPTV (Plex, Pluto, etc.)",
+    "url": "https://raw.githubusercontent.com/Free-TV/IPTV/master/playlist.m3u8"
+  },
+  {
+    "id": "pluto-tv",
+    "label": "Pluto TV (250+ channels)",
+    "url": "https://i.mjh.nz/PlutoTV/all.m3u8"
+  },
+  {
+    "id": "samsung-tv-plus",
+    "label": "Samsung TV Plus (200+ channels)",
+    "url": "https://raw.githubusercontent.com/BuddyChewChew/app-m3u-generator/main/playlists/samsungtvplus_us.m3u"
+  },
+  {
+    "id": "plex-tv",
+    "label": "Plex TV Live Channels",
+    "url": "https://raw.githubusercontent.com/BuddyChewChew/app-m3u-generator/main/playlists/plex_us.m3u"
+  },
+  {
+    "id": "roku-channel",
+    "label": "Roku Channel (300+ channels)",
+    "url": "https://raw.githubusercontent.com/BuddyChewChew/app-m3u-generator/main/playlists/roku_all.m3u"
+  },
+  {
+    "id": "vizio-tv",
+    "label": "Vizio TV (Kids, News, Food)",
+    "url": "https://www.apsattv.com/vizio.m3u"
+  },
+  {
+    "id": "xiaomi-tv",
+    "label": "Xiaomi TV+ (250+ channels)",
+    "url": "https://www.apsattv.com/xiaomi.m3u"
+  },
+  {
+    "id": "distrotv",
+    "label": "DistroTV (250+ channels)",
+    "url": "https://www.apsattv.com/distro.m3u"
+  },
+  {
+    "id": "local-now",
+    "label": "Local Now (News, Weather, Lifestyle)",
+    "url": "https://www.apsattv.com/localnow.m3u"
+  },
+  {
+    "id": "lg-channels",
+    "label": "LG Channels (1,000+ channels)",
+    "url": "https://www.apsattv.com/lg.m3u"
+  },
+  {
+    "id": "tablo",
+    "label": "Tablo (150+ channels)",
+    "url": "https://www.apsattv.com/tablo.m3u"
+  },
+  {
+    "id": "fire-tv",
+    "label": "Fire TV (50 channels)",
+    "url": "https://www.apsattv.com/firetv.m3u"
+  },
+  {
+    "id": "redbox-tv",
+    "label": "Redbox TV",
+    "url": "https://www.apsattv.com/redbox.m3u"
+  },
+  {
+    "id": "xumo",
+    "label": "Xumo (350+ channels)",
+    "url": "https://www.apsattv.com/xumo.m3u"
+  },
+  {
+    "id": "news-channels",
+    "label": "News Channels",
+    "url": "https://iptv-org.github.io/iptv/categories/news.m3u"
+  },
+  {
+    "id": "sports-channels",
+    "label": "Sports Channels",
+    "url": "https://iptv-org.github.io/iptv/categories/sports.m3u"
+  },
+  {
+    "id": "movie-channels",
+    "label": "Movie Channels",
+    "url": "https://iptv-org.github.io/iptv/categories/movies.m3u"
+  },
+  {
+    "id": "music-channels",
+    "label": "Music Channels",
+    "url": "https://iptv-org.github.io/iptv/categories/music.m3u"
+  },
+  {
+    "id": "documentary-channels",
+    "label": "Documentary Channels",
+    "url": "https://iptv-org.github.io/iptv/categories/documentary.m3u"
+  },
+  {
+    "id": "entertainment-channels",
+    "label": "Entertainment Channels",
+    "url": "https://iptv-org.github.io/iptv/categories/entertainment.m3u"
+  }
 ]
 
 const CACHE_KEY = 'iptv_channels_cache'
